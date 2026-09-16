fea31f061c85b936afebf457dd410b7e361ef4a5
= <=>
2026-09-16 01:37:30 -0600

    Add BufferManager registration to service collection

--- src/Nexus.Runtime/ServiceCollectionExtensions.cs
+++ src/Nexus.Runtime/ServiceCollectionExtensions.cs
@@ -14,6 +14,7 @@ public static class ServiceCollectionExtensions
         services.TryAddSingleton<ISyncManager, SyncManager>();
         services.TryAddSingleton<IPipelineFactory, PipelineFactory>();
         services.TryAddSingleton<IPipelineRegistry, PipelineRegistry>();
+        services.TryAddSingleton<IBufferManager, BufferManager>();
         services.TryAddSingleton<IDescriptorSetPool, DescriptorSetPool>();
         services.TryAddSingleton<IRenderer, Renderer>();
         services.TryAddSingleton<ISwapChain, SwapChain>();
