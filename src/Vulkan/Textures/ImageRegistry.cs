475953f4da5423fdd76c3c7a0ddd20b38292583c
= <=>
2026-09-22 12:56:45 -0600

    Refactor ImageRegistry: update image and view management, implement Release and Reset methods, and enhance resource cleanup

--- src/Vulkan/Textures/ImageRegistry.cs
+++ src/Vulkan/Textures/ImageRegistry.cs
@@ -2,10 +2,12 @@ namespace Nexus.Graphics.Vulkan.Textures;
 
 public class ImageRegistry : IImageRegistry
 {
-    private readonly Dictionary<(TextureId TextureId, ColorFormatEnum Format), VkImage> _images =
-    [];
+    private readonly Dictionary<ulong, VkImage> _images = [];
     private readonly Dictionary<VkImage, DeviceMemory> _memory = [];
-    private readonly Dictionary<(VkImage, ViewDefinition), List<VkImageView>> _views = [];
+
+    private readonly Dictionary<ulong, VkImageView> _views = [];
+    private readonly Dictionary<VkImage, List<ulong>> _imageViews = [];
+
     private readonly Dictionary<VkImage, int> _refs = [];
     private readonly Queue<VkImage>[] _released;
 
@@ -28,18 +30,71 @@ public class ImageRegistry : IImageRegistry
     public IEnumerable<IVulkanCommand> Release(ITexture texture)
     {
         ArgumentNullException.ThrowIfNull(texture);
-        throw new NotImplementedException();
+
+        foreach (var pair in _images.Where(x => x.Key.TextureId == texture.Id).ToArray())
+        {
+            var image = pair.Value;
+
+            if (!_refs.TryGetValue(image, out var refs))
+                continue;
+
+            if (refs > 1)
+            {
+                _refs[image] = refs - 1;
+                continue;
+            }
+
+            _refs.Remove(image);
+            _images.Remove(pair.Key);
+
+            QueueRelease(image);
+        }
+
+        return [];
+    }
+
+    private void Destroy(VkImage image)
+    {
+        if (_imageViews.Remove(image, out var viewIds))
+        {
+            foreach (var viewId in viewIds)
+            {
+                if (_views.Remove(viewId, out var view))
+                    _context.Vk.DestroyImageView(_context.Device, view, null);
+            }
+        }
+
+        _context.Vk.DestroyImage(_context.Device, image, null);
+
+        if (_memory.Remove(image, out var memory))
+            _context.Vk.FreeMemory(_context.Device, memory, null);
     }
 
     public void Reset()
     {
-        throw new NotImplementedException();
+        foreach (var image in _memory.Keys.ToArray())
+            Destroy(image);
+
+        _images.Clear();
+        _refs.Clear();
+        _views.Clear();
+        _imageViews.Clear();
+
+        foreach (var queue in _released)
+            queue.Clear();
     }
 
     public void Dispose()
     {
-        throw new NotImplementedException();
+        _syncManager.FrameCompleted -= OnFrameCompleted;
+
+        Reset();
+
+        GC.SuppressFinalize(this);
     }
+
+    private static ulong ComputeImageId(TextureId id, ColorFormatEnum color) =>
+        new IdentityHashBuilder("ImageId").Add(id).Add((uint)color).Compute();
 
     private static ulong ComputeImageViewId(in ImageViewCreateInfo info) =>
         new IdentityHashBuilder("ImageViewId")
